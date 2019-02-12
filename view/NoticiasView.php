19a1df5043ae315ce10899a5fc5358ec4b236b24
FacuGenaro <facundogenaro@hotmail.com>
2019-02-12 03:57:27 -0300

    Fix

--- view/NoticiasView.php
+++ view/NoticiasView.php
@@ -11,7 +11,7 @@ class NoticiasView
     $this->Smarty->assign('titulo', $titulo);
     $this->Smarty->assign('noticia', $arrNoticias);
     $this->Smarty->assign('index', "http://".$_SERVER["SERVER_NAME"].dirname($_SERVER["PHP_SELF"]));
-    $this->Smarty->display('templates/index.tpl');
+    $this->Smarty->display('templates/indexLogueado.tpl');
   }
 
   // function mostrarPaginaVisitante($titulo, $arrNoticias){
